005c0fbbb7731c1677fa44c03a1a87a5f7c7c43f
stupxd <uknownvector@gmail.com>
2024-07-23 00:50:23 +0300

    make mod steamodded independed, fix error with modded jokers without name (by ItsFlowwey)

--- StickersAlwaysShown.lua
+++ StickersAlwaysShown.lua
@@ -1,16 +1,4 @@
---- STEAMODDED HEADER
---- MOD_NAME: Stickers Always Shown 🃏
---- MOD_ID: StickersAlwaysShown
---- MOD_AUTHOR: [SirMaiquis]
---- MOD_DESCRIPTION: The jokers stickers are always shown!
-
-----------------------------------------------
-------------MOD CODE -------------------------
-
-function SMODS.INIT.StickersAlwaysShown()
-    --- Enable achivements, you can comment it if you dont want them.
-    G.F_NO_ACHIEVEMENTS = false
-end
+G.F_NO_ACHIEVEMENTS = false
 
 local CardDrawRef = Card.draw
 function Card:draw(layer)
@@ -27,19 +15,10 @@ function Card:draw(layer)
 end
 
 function GetSticker(card)
-    if(card.sticker) then return card.sticker end
-        local center = GetCenterKeyByJokerName(card.ability.name)
+    if (card.sticker) then return card.sticker end
+        local center = card.config.center
         local sticker = get_joker_win_sticker(center)
         return sticker
-end
-
-function GetCenterKeyByJokerName(name)
-    for key, joker in pairs(G.P_CENTERS) do
-        if joker.name == name then
-            return joker
-        end
-    end
-    return nil
 end
 
 local win_gameRef = win_game
@@ -50,6 +29,3 @@ function win_game()
     end
     return (t)
 end
-
-----------------------------------------------
-------------MOD CODE END----------------------
---
+++ mod.lua
@@ -0,0 +1,13 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Stickers Always Shown 🃏
+--- MOD_ID: StickersAlwaysShown
+--- MOD_AUTHOR: [SirMaiquis]
+--- MOD_DESCRIPTION: The jokers stickers are always shown!
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
